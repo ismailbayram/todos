6cc5a402bd979242e74e67df4f90c5251fbf0bef
ismail bayram <ismailbayram.ce@gmail.com>
2022-12-31 17:32:39 +0300

    added default admin user to migration

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ismailbayram/todos/src/config"
 	"github.com/ismailbayram/todos/src/database"
 	"github.com/ismailbayram/todos/src/users"
+	"log"
 )
 
 func main() {
@@ -11,4 +12,11 @@ func main() {
 	db := database.New(&cfg.Database)
 	models := []interface{}{users.User{}}
 	db.Migrate(models)
+
+	db = database.New(&cfg.Database)
+	ur := users.NewUserRepository(db.Conn)
+	_, err := ur.Create("admin", "123456", true)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
